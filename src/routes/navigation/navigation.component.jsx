24d43683632a6a4de49408e7caf46047c72015d9
TheKing0000 <simon.kovesdi2@gmail.com>
2022-06-26 10:25:32 +0200

    routing implementationj

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom"
+import { Fragment } from "react";
+//we dont need a div , but we need a parent element
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg"
+
+import "./navigation.styles.scss"
+//you can import (the svg) it as a component
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">SHOP</Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
+export default Navigation;
